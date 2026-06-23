f03f87704e2acd6b83487b4d55fda505b8a61e72
Farul-Ikhwan <farul3306@gmail.com>
2026-06-24 06:39:09 +0800

    Update login.php

--- client/login.php
+++ client/login.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../config.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
+
 
 header('Content-Type: application/json');
 
